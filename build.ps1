9028b474a8a2656f86bd0f39fa1d027e5b79dd2a
Shaswat Rungta <shrung@microsoft.com>
2021-05-31 01:37:59 +0530

    Update build and run files

--- build.ps1
+++ build.ps1
@@ -1,8 +1,1 @@
-# Bootstrap the project
-# docker run --rm -v ${PWD}/blog:/srv/jekyll/blog jekyll/jekyll:latest jekyll new . 
-
-# Run the site
-# docker run -v ${PWD}:/srv/jekyll -p 4000:4000 -it jekyll/jekyll:latest jekyll serve
-
-# Run the site without refresh
-docker run --rm -v ${PWD}:/srv/jekyll -p 4000:4000 -e JEKYLL_ENV=production -it jekyll/jekyll:latest jekyll serve --force_polling 
+docker build . -t srungtagithub:debug
